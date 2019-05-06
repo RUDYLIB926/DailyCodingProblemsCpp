c22d6eca47c919ff6d2fccba9a0c88b59ec38390
Rudolph Libertini <rlibertini3@gmail.com>
2019-05-06 13:31:53 -0400

    Changed the main program to ask for a directory path

--- src/ProblemSelector.cpp
+++ src/ProblemSelector.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <filesystem>
+//#include <experimental/filesystem>
 #include <locale>
 #include <codecvt>
 #include <string>
@@ -8,35 +9,35 @@
 #include "ProblemSelector.h"
 
 // private function declarations
-bool checkIfProblemWasSpecified(std::string _problemFilePath);
-bool tryRunExistingProblem(std::string _problemFilePath);
-int runExistingProblem(LPCSTR problemName);
+bool checkIfProblemWasSpecified(std::string problem);
+bool tryRunExistingProblem(std::string problem);
+int runExistingProblem(LPCSTR problem);
 std::vector<std::string> split(const std::string& s, char delimiter);
 void printList(std::vector<std::string> list);
 
 // ------------------------------------------------------------------------------------------------------------
 bool ProblemSelector::InitializeProblem(){
-    if (!checkIfProblemWasSpecified(_problemFilePath)){
+    if (!checkIfProblemWasSpecified(_problemFilePath + _problemName)){
         return false;
     }
-    else return tryRunExistingProblem(_problemFilePath);
+    else return tryRunExistingProblem(_problemFilePath + _problemName);
 }
 
-bool checkIfProblemWasSpecified(std::string _problemFilePath){
-    if (_problemFilePath.empty()){
+bool checkIfProblemWasSpecified(std::string problem){
+    if (problem.empty()){
         std::cout << "No problem was specified: did not initialize." << std::endl;
         return false;
     }
     return true;
 }
 
-bool tryRunExistingProblem(std::string _problemFilePath){
-    int success = runExistingProblem(_problemFilePath.c_str());
+bool tryRunExistingProblem(std::string problem){
+    int success = runExistingProblem(problem.c_str());
     if (success != 0) return false;
     else return true;
 }
 
-int runExistingProblem(LPCSTR problemName){
+int runExistingProblem(LPCSTR problem){
     STARTUPINFO si;
     PROCESS_INFORMATION pi;
 
@@ -44,7 +45,7 @@ int runExistingProblem(LPCSTR problemName){
     si.cb = sizeof(si);
     ZeroMemory( &pi, sizeof(pi) );
 
-    if (!CreateProcess(problemName, NULL, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
+    if (!CreateProcess(problem, NULL, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
     {
         printf( "CreateProcess failed (%d).\n", GetLastError() );
         return -1;
@@ -62,7 +63,7 @@ int runExistingProblem(LPCSTR problemName){
 // ------------------------------------------------------------------------------------------------------------
 void ProblemSelector::DisplayAllProblems(){
      try{
-        std::string path("C:\\Users\\rlibe\\source\\repos\\DailyCodingProblems\\DailyCodingProblemsCpp\\Problems\\");
+        std::string path = _problemFilePath;
         std::string ext(".exe");
         std::vector<std::string> problemList;
         for (const auto & entry : std::filesystem::directory_iterator(path)){
@@ -75,7 +76,7 @@ void ProblemSelector::DisplayAllProblems(){
         printList(problemList);
 
     }catch(...){
-        std::cout << "Error: Could not list all problems. Look in ProblemSelctor.cpp listProblems() function.";
+        std::cout << "Error: Could not list all problems. Look in ProblemSelctor.cpp listProblems() function." << std::endl;
     }
 }
 
@@ -97,7 +98,10 @@ void printList(std::vector<std::string> list){
 
 // ------------------------------------------------------------------------------------------------------------
 void ProblemSelector::SetNewProblem(std::string problemName){
-     _problemFilePath = 
-        "C:\\Users\\rlibe\\source\\repos\\DailyCodingProblems\\DailyCodingProblemsCpp\\Problems\\"
-        + problemName;
+     _problemName = problemName;
+}
+
+// ------------------------------------------------------------------------------------------------------------
+void ProblemSelector::SetPath(std::string path){
+    _problemFilePath = path;
 }
--- src/Program.cpp
+++ src/Program.cpp
@@ -3,22 +3,40 @@
 #include <windows.h>
 #include "ProblemSelector.h"
 
+std::string normalizePath(std::string path);
+
 int main()
 {
     std::string problem = "";
+    std::string path = "";
+    std::cout << "Enter the directory path to the problems: " << std::endl;
+    getline(std::cin, path);
+    path = normalizePath(path);
     ProblemSelector selector;
+    selector.SetPath(path);
+    std::cout << std::endl;
 
     while (true){
         selector.DisplayAllProblems();
         std::cout << "Enter the problem you want to open: ";
         std::cin >> problem;
+        std::cout << std::endl;
         if (problem == "EXIT") break;
         selector.SetNewProblem(problem);
         if (selector.InitializeProblem()){
             std::cout << "Problem was executed." << std::endl
                 << "type EXIT to quit, or..." << std::endl;
         }
+        std::cout << std::endl;
     }
     return 0;
 }
 
+std::string normalizePath(std::string path){
+    std::string normalizePath = "";
+    // ToDo: algorithm to noramlize the path
+    //std::string path("C:\\Users\\rlibe\\source\\repos\\DailyCodingProblems\\DailyCodingProblemsCpp\\Problems\\");
+    //std::string path("C:\\Users\\rlibertini\\Source\\Repos\\DailyCodingProblemsCpp\\Problems\\");
+    normalizePath = "C:\\Users\\rlibertini\\Source\\Repos\\DailyCodingProblemsCpp\\Problems\\";
+    return normalizePath;
+}
